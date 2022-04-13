7cd9b7f53b178b0b2e8df35d3c861574ed0093ca
Alon W <alws34@gmail.com>
2022-04-13 08:27:25 -0900

    prep_data work: got the data in the desired shape (df) in order to aggregate (implemented create_sequence())

--- FlaskServer/flasktest.py
+++ FlaskServer/flasktest.py
@@ -1,5 +1,5 @@
-#import pandas as pd
-# import numpy as np
+import pandas as pd
+import numpy as np
 import datetime
 import os, sys, shutil
 from flask import Flask, jsonify, request
@@ -20,6 +20,9 @@ N_PAST = 1 # days on which the model was train to precict based on.
 MAX_TWEETS_RESULTS = 100 # max results for first tweets query.
 MIN_TWEET_STATS_SUM = 100
 MIN_USER_FOLLOWERS = 100 # min user followers num to be included
+
+pd.set_option('display.max_columns', None)
+
 
 def connect_to_twitter(version = TWITTER_VERSION): # v2 for now
     consumer_key = 'mymakG2knztQ2GYaTNaRGTIOi'
@@ -43,9 +46,22 @@ def get_n_past_date(num_of_days):
     today = datetime.datetime.now()
     if num_of_days > 0 :
         num_of_days *= -1
+    if num_of_days != 0: 
+        num_of_days += 1 # remove 1 less day for taking TODAY into account (in oppose to the model training)
     delta = datetime.timedelta(num_of_days)
     date = today + delta
+
     return date.isoformat('T')+'Z'
+
+def create_sequence(dataset, rows_at_a_time):
+    sequences = []
+    start_idx = 0
+    features_df = dataset
+    for stop_idx in range(rows_at_a_time,len(dataset)): 
+        sequences.append(features_df.iloc[start_idx:stop_idx])
+        start_idx += 1
+    return np.array(sequences)
+
 
 
 # Step 1
@@ -70,12 +86,10 @@ def get_tweets(ticker, max_results = 10, n_past = 1, twitter_version=TWITTER_VER
                 'n_likes':t['favorite_count'],
                 'n_retweets':t['retweet_count'],
                 'n_replies': 0,
-                'user': {
-                    'user_id':user['id'],
-                    'screen_name':user['screen_name'],
-                    'description':user['description'],
-                    'n_followers':user['n_followers']
-                }
+                'u_id':user['id'],
+                'u_screen_name':user['screen_name'],
+                'u_description':user['description'],
+                'u_n_followers':user['n_followers']
             })
     elif twitter_version == 2:
         prms = {
@@ -105,12 +119,10 @@ def get_tweets(ticker, max_results = 10, n_past = 1, twitter_version=TWITTER_VER
                     'n_likes':t_metrics['like_count'],
                     'n_retweets':t_metrics['retweet_count'],
                     'n_replies': t_metrics['reply_count'],
-                    'user': {
-                        'user_id':u_data['id'],
-                        'screen_name':u_data['username'],
-                        'description':u_data['description'],
-                        'n_followers':u_data['public_metrics']['followers_count']
-                    }
+                    'u_id':u_data['id'],
+                    'u_screen_name':u_data['username'],
+                    'u_description':u_data['description'],
+                    'u_n_followers':u_data['public_metrics']['followers_count']
                 })
         except Exception as err:
             print("HERE!")
@@ -122,7 +134,8 @@ def get_tweets(ticker, max_results = 10, n_past = 1, twitter_version=TWITTER_VER
 def get_sentiment(tweets):
     print("Getting Sentiment")
     for tweet in tweets:
-        tweet['sentiment'] = sentiment_analyzer.polarity_scores(tweet['text'])
+        s = sentiment_analyzer.polarity_scores(tweet['text'])
+        tweet['s_neg'], tweet['s_neu'], tweet['s_pos'], tweet['s_compound'] = s['neg'], s['neu'], s['pos'], s['compound']
         #tweets['subjectivity'] = sentiment_analyzer.subjectivity_scores(tweet['text']) # optional
     return tweets
 
@@ -130,7 +143,7 @@ def get_sentiment(tweets):
 def filter_tweets(tweets, threshold = MIN_TWEET_STATS_SUM):
     print("Filtering Tweets")
     for tweet in tweets:
-        if tweet['n_retweets'] + tweet['n_likes'] + tweet['n_replies'] < threshold or tweet['sentiment']['compound'] == 0:
+        if tweet['n_retweets'] + tweet['n_likes'] + tweet['n_replies'] < threshold or tweet['s_compound'] == 0:
             tweets.remove(tweet)
     return tweets
 
@@ -148,8 +161,8 @@ def get_users_engagement(tweets, max_tweets_results = 100, twitter_version=TWITT
 
         for i,tweet in enumerate(tweets):
             try:
-                u_tweets = twitter.request(resource='users/:'+tweet['user']['user_id']+'/tweets', params = prms)
-                u_log_n_followers = math.log(tweet['user']['n_followers'],2)
+                u_tweets = twitter.request(resource='users/:'+tweet['u_id']+'/tweets', params = prms)
+                u_log_n_followers = math.log(tweet['u_n_followers'],2)
                 u_n_tweets, u_n_rts, u_n_replies, u_n_likes = 0, 0, 0, 0
                 for u_tweet in u_tweets:
                     u_n_rts += u_tweet['public_metrics']['retweet_count']
@@ -160,39 +173,54 @@ def get_users_engagement(tweets, max_tweets_results = 100, twitter_version=TWITT
                 if math.log(u_log_n_followers,2) > 0 and u_n_tweets > 0:
                     eng = (u_n_rts + u_n_replies + u_n_likes)/math.log(u_log_n_followers,2)/u_n_tweets
                 else: eng = 0
-                tweets[i]['user']['engagement'] = eng
+                tweets[i]['u_engagement'] = eng
             
             except TwitterRequestError as err:  
                 if "429" in str(err):
                     err_msg = "429 in err"
                     print(err_msg, err)
-                    tweets[i]['user']['engagement'] = err_msg
+                    tweets[i]['u_engagement'] = 0
                     #time.sleep(15*60) # sleep 15 mins
                 else:
                     err_msg = "twitter request error"
                     print(err_msg, err)
-                    tweets[i]['user']['engagement'] = err_msg
+                    tweets[i]['u_engagement'] = 0
             except Exception as err:
                 err_msg = "general exception"
                 print(err_msg, err)
-                tweets[i]['user']['engagement'] = err_msg
+                tweets[i]['u_engagement'] = 0
 
     return tweets
 
 # Step 5
 def filter_users(tweets,threshold=MIN_USER_FOLLOWERS):
     print("Filtering users")
-
     for tweet in tweets:
-        user = tweet['user']
-        if user['engagement'] == 0 or user['n_followers'] < threshold:
+        if tweet['u_n_followers'] < threshold or tweet['u_engagement'] == 0:
              tweets.remove(tweet)
     return tweets
 
 # Step 6
 def prep_data(data):
     print("Prepping model data")
-    pass
+
+    keys = data[0].keys()
+    temp = {}
+    for key in keys:
+        temp[key] = []
+        for d in data:
+            temp[key].append(d[key])
+    print(temp)
+    df = pd.DataFrame.from_dict(temp)
+
+    features = ['n_replies', 'n_retweets', 'n_likes', 's_pos', 's_neg', 's_neu', 'u_engagement']
+    df = df[features]
+    print('before mean', df)
+    df = df.mean() # problematic
+    print(df, len(df))
+    test_seq = create_sequence(df, rows_at_a_time=N_PAST)
+    print(test_seq,test_seq.shape)
+    return data
 
 
 
@@ -202,7 +230,7 @@ api = Api(app)
 
 #debug
 def get_prediction_dummy():
-    ticker = "TSLA"
+    ticker = "MSFT"
     # Step 1
     tweets = get_tweets('TSLA',max_results=MAX_TWEETS_RESULTS,n_past=N_PAST, twitter_version=TWITTER_VERSION)
 
@@ -219,10 +247,11 @@ def get_prediction_dummy():
 
     # Step 5
     print("before user filtering:", len(eng_filteredT_sent_tweets))
-    filteredU_eng_filteredT_sent_tweets = filter_users(eng_filteredT_sent_tweets)
+    filteredU_eng_filteredT_sent_tweets = filter_users(eng_filteredT_sent_tweets,threshold=MIN_USER_FOLLOWERS)
     print("after user filtering:", len(filteredU_eng_filteredT_sent_tweets))
     
-
+    # Step 6
+    preped = prep_data(filteredU_eng_filteredT_sent_tweets)
 
 @app.route('/getPrediction', methods=['GET'])  # GET
 def get_prediction():
@@ -243,3 +272,4 @@ if __name__ == '__main__':
     print("3")
     get_prediction_dummy() # debug
     app.run(host='0.0.0.0', port=SERVER_PORT, debug=True)
+
